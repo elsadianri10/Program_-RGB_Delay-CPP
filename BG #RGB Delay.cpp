f2ce0083d8f2d35f0624a662a4fa5100081fb9f7
Elsa Diana Putri <elsadianri10@gmail.com>
2019-07-28 09:19:17 +0700

    Add files via upload

---
+++ BG #RGB Delay.cpp
@@ -0,0 +1,43 @@
+# include <iostream>
+# include <windows.h> // Sleep( milliseconds );
+
+using namespace std;
+
+int main()
+{
+	system( "color 01" );
+    cout <<"\n\n\n\n\t\t\t\tLu mau punya pacar peka? Pacarin aja Dev C++";
+
+    system( "color 10" );
+    Sleep( 2000 );
+    
+    system( "color 20" );
+    Sleep( 2000 );
+    
+    system( "color 30" );
+    Sleep( 2000 );
+    
+    system( "color 40" );
+    Sleep( 2000 );
+    
+    system( "color 50" );
+    Sleep( 2000 );
+    
+    system( "color 60" );
+    Sleep( 2000 );
+    
+    system( "color 70" );
+    Sleep( 2000 );
+    
+    system( "color 80" );
+    Sleep( 2000 );
+    
+    system( "color 90" );
+    Sleep( 2000 );
+    
+ 
+    
+ 
+ system ( "pause" );   
+    
+}
---
+++ Text #RGB Delay.cpp
@@ -0,0 +1,93 @@
+# include <iostream>
+# include <windows.h> // Sleep( milliseconds );
+
+using namespace std;
+
+int main()
+{
+    
+    cout << "\n\n\t\tMalming ku dengan tugas, kalo kalian?";
+    
+    system ( "color 01" );
+    Sleep ( 1000 );
+    
+    system ( "color 02" );
+    Sleep ( 1000 );
+    
+    system ( "color 03" );
+    Sleep ( 1000 );
+    
+    system ( "color 04" );
+    Sleep ( 1000 );
+    
+    system ( "color 05" );
+    Sleep ( 1000 );
+    
+    system ( "color 06" );
+    Sleep ( 1000 );
+    
+    system ( "color 07" );
+    Sleep ( 1000 );
+    
+    system ( "color 08" );
+    Sleep ( 1000 );
+    
+    system ( "color 09" );
+    Sleep ( 1000 );
+    
+    system( "color 0A" );
+    Sleep( 1000 );
+    
+    system( "color 0B" );
+    Sleep( 1000 );
+    
+    system( "color 0C" );
+    Sleep( 1000 );
+    
+    system( "color 0D" );
+    Sleep( 1000 );
+    
+    system( "color 0E" );
+    Sleep( 1000 );
+    
+    system( "color 0F" );
+    Sleep( 1000 );
+    
+    system ( "cls" );
+    
+    system ( "color 01" );
+    cout << "\n\n\t\tMalming ku dengan tugas, kalo kalian?";
+
+    system ( "color 10" );
+    Sleep( 1000 );
+    
+    system ( "color 20" );
+    Sleep( 1000 );
+    
+    system ( "color 30" );
+    Sleep( 1000 );
+    
+    system( "color 40" );
+    Sleep( 1000 );
+    
+    system( "color 50" );
+    Sleep( 1000 );
+    
+    system ( "color 60" );
+    Sleep( 1000 );
+    
+    system( "color 70" );
+    Sleep( 1000 );
+    
+    system( "color 80" );
+    Sleep( 1000 );
+    
+    system( "color 90" );
+    Sleep( 1000 );
+    
+ 
+    
+ 
+ system ( "pause" );   
+    
+}
